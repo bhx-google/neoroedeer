e53390cd738f570a34a72de96a11e936e00cd6bc
Bill Xing <bhx@google.com>
2022-01-20 19:07:03 +0000

    added scripts to generate tmux config file.

---
+++ scripts/get_window.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+local script_dir=`dirname "$(realpath $0)"`
+source $script_dir/piper_helper.sh
+
+get_window_name
---
+++ scripts/piper_helper.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+export cloudHome="/google/src/cloud/$USER"
+
+# Workspace name
+function workspace() {
+  # Use :A parameter expansion to resolve symlinks to /google/src
+  # http://zsh.sourceforge.net/Doc/Release/Expansion.html#index-parameter-expansion-flags
+  if [[ $PWD:A =~ "$cloudHome/([^/]+)" ]]; then
+    echo ${match[1]}
+  fi
+}
+
+# Absolute directory of our workspace
+function workspace_dir() {
+  if [[ $WORKSPACE != "" ]]; then
+    echo "$cloudHome/$WORKSPACE/google3"
+  fi
+}
+
+# Relative path after google3/
+function source_pwd() {
+  local wkspace_dir=$(workspace_dir)
+  if [[ -n "$wkspace_dir" ]]; then
+    echo "$PWD[${#wkspace_dir}+2,${#PWD}]"
+  fi
+}
+
+function get_window_name() {
+  local name=$(workspace)
+  if [[ -z $name ]]; then
+    basename $PWD
+  else
+    echo "W:$name"
+  fi
+}
