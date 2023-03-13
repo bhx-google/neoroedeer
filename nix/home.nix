63d82f4bb8c02ff6ab041a9053a18a15d5fd47bc
Bill Xing <bhx@google.com>
2023-03-13 19:13:42 +0000

    Initial nix config.

---
+++ nix/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = [
+	pkgs.neovim
+  ];
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "bhx";
+  home.homeDirectory = "/usr/local/google/home/bhx";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
