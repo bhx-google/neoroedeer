900c45b2afdf5fe71ed979d336c0d7904b7e7be1
Bill Xing <bhx@google.com>
2025-02-18 19:40:49 +0000

    Update modified file.

--- nix/install.sh
+++ nix/install.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 mkdir -p ~/.config/nixpkgs/
-ln -sf $SCRIPT_DIR/home.nix ~/.config/nixpkgs/home.nix
+ln -sf $SCRIPT_DIR/home.nix /usr/local/google/home/bhx/.config/home-manager/home.nix
 
