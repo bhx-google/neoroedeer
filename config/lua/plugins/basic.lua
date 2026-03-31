73856968e0c460ef981c63610490ef669261e88b
Bill Xing <bhx@google.com>
2026-03-31 21:47:25 +0000

    [nvim] add pdf viewer.

--- config/lua/plugins/basic.lua
+++ config/lua/plugins/basic.lua
@@ -198,4 +198,15 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
     opts = {},
   },
+  {
+    "r-pletnev/pdfreader.nvim",
+    lazy = false,
+    dependencies = {
+      "folke/snacks.nvim", -- image rendering
+      "nvim-telescope/telescope.nvim", -- pickers
+    },
+    config = function()
+      require("pdfreader").setup()
+    end,
+  },
 }
